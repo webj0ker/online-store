8428ab802de43cf37e50d20b43f5decab1b686ea
Stas <stanislav.shendryk@gmail.com>
2022-12-30 21:15:56 +0200

    fix: remove spaces product link

--- src/components/view/products/products.ts
+++ src/components/view/products/products.ts
@@ -63,7 +63,7 @@ class Products {
       productsClone
         .querySelector<HTMLElement>('.products__read-more a')
         // ?.setAttribute('href', item.images[0])
-        ?.setAttribute('href', `product/${item.title}`)
+        ?.setAttribute('href', `product/${item.title.split(' ').join('')}`)
       fragment.append(productsClone)
     })
     setElement('.products', 'innerHTML', '')
